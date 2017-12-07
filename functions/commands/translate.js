065a1522b2f9f066f5d511ddfc698198eb89e0f1
connieli <c.ziye.li@gmail.com>
2017-12-06 22:43:26 -0500

    delete logs and comments

--- functions/commands/translate.js
+++ functions/commands/translate.js
@@ -9,16 +9,8 @@ const han = require('han')
 const hanzi = require("hanzi");
 hanzi.start();
 
-// const translate = require('google-translate-api');
-// const GoogleTranslate = require('@google-cloud/translate');
-// const googleTranslate = require('@google-cloud/translate')({
-//   key: process.env.GOOGLE_TRANSLATE_API_KEY
-// });
 const request = require('superagent');
 const YANDEX_KEY = process.env.YANDEX_KEY;
-
-// only works for english :()
-// const PUNCT_REGEX = /[^\.\!\?]*[\.\!\?]/g;
 
 /**
 * GET /translate
@@ -43,7 +35,6 @@ module.exports = (user, channel, text = '', command = {}, botToken = null, callb
     }) // sends a JSON post body
     .end(function(err, res){
       if (err) {
-        console.log(" ========= Yandex ERROR ========= ", err);
         utils.log.error("error message", new Error("yandex error"), (err) => {
           // Handle error
           callback(null, {
@@ -54,34 +45,12 @@ module.exports = (user, channel, text = '', command = {}, botToken = null, callb
       }
       // Calling the end function will send the request
       const resp = res.body;
-      
       callback(null, {
         response_type: 'in_channel',
         text: resp.text[0], // full english translation
         "attachments": generateSentenceTranslations(text)
       });
     });
-
-  // googleTranslate
-  //   .translate(text, 'en')
-  //   .then((results) => {
-  //     let translations = results[0];
-  //     translations = Array.isArray(translations) ? translations : [translations];
-
-  //     let firstTranslation = translations[0];
-  //     translations.forEach((translation, i) => {
-  //       console.log(`${text[i]} => (${translation}`);
-  //     });
-
-  //     callback(null, {
-  //       response_type: 'in_channel',
-  //       text: `${firstTranslation}`,
-  //       "attachments": generateSentenceTranslations(text)
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     console.error('ERROR:', err);
-  //   });
 };
 
 /**
@@ -93,7 +62,6 @@ function generateSentenceTranslations(text) {
   const sentences = text.split(' ')
 
   sentences.forEach(sentence => {
-    // const han_pinyin = han.pinyin(sentence);
     const pair = translateSentenceToPinyin(sentence);
     const segments_original = pair[0];
     const segments_pinyin_arr = pair[1];
@@ -117,14 +85,10 @@ function generateSentenceTranslations(text) {
 function translateSentenceToPinyin(text) {
   // split into segments then join with 
   const segments = hanzi.segment(text);
-  // console.log("sentence segments", segments);
   // convert each segment to pinyin 
   const han_segments = segments.map(segment => {
     const definition = hanzi.definitionLookup(segment, 's');
-    // const hanzi_pinyin = hanzi.getPinyin(segment);
     const han_pinyin = han.pinyin(segment);
-    // console.log("def", definition);
-    // console.log("===== han_pinyin ===== ", han_pinyin);
     return pinyinArrayToString(han_pinyin, '')
   });
   // const han_segments_str = han_segments.join(' · ')
